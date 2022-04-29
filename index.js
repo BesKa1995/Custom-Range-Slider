2507d63215f0e0fea38cc1d5780b390a7ea094eb
beso <beskapoladishvili@gmail.com>
2022-04-29 23:14:23 +0400

    added comments

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ range.addEventListener('input', (e) => {
 
   const label = range.nextElementSibling
   label.textContent = value
-
+  //taking range and label width values
   const rangeWidth = getComputedStyle(e.target).getPropertyValue('width')
   const labelWidth = getComputedStyle(label).getPropertyValue('width')
-
+  //converting css values to int
   const numRangeWidth = +rangeWidth.substring(0, rangeWidth.length - 2)
   const numLableWidth = +labelWidth.substring(0, labelWidth.length - 2)
-
+  //max min values of range
   const max = +e.target.max
   const min = +e.target.min
-
+  //math calculation for label to track slider thumb
   const left = value * (numRangeWidth / max) - numLableWidth / 2
 
   label.style.left = `${left}px`
